50607c0de4fefca055b49b33d13bbe24a6c5fab0
Emmanuel <emmanueldurin@gmail.com>
2022-01-07 10:26:51 +0100

    Requetes en DB du controleur

--- DemoWeatherForeCast/Controllers/WeatherForecastController.cs
+++ DemoWeatherForeCast/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,43 +14,51 @@ namespace DemoRestRentACar.Controllers
   [Route("[controller]")]
   public class WeatherForecastController : ControllerBase
   {
-    private static string[] Summaries = new[]
-    {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-    private static List<WeatherForecast> weatherForecasts;
-    static WeatherForecastController()
-    {
-      var rng = new Random();
-      weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-      {
-        Id = index,
-        Place="Pommiers",
-        Date = DateTime.Now.AddDays(index),
-        TemperatureC = rng.Next(-20, 55),
-        Summary = Summaries[rng.Next(Summaries.Length)]
-      })
-      .ToList();
-    }
-
+    //private static string[] Summaries = new[]
+    //{
+    //        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    //    };
+    //private static List<WeatherForecast> weatherForecasts;
+    //static WeatherForecastController()
+    //{
+    //  var rng = new Random();
+    //  weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    //  {
+    //    Id = index,
+    //    Place="Pommiers",
+    //    Date = DateTime.Now.AddDays(index),
+    //    TemperatureC = rng.Next(-20, 55),
+    //    Summary = Summaries[rng.Next(Summaries.Length)]
+    //  })
+    //  .ToList();
+    //}
     private readonly IConfiguration _configuration;
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly WeatherForecastContext _context;
     public WeatherForecastController(WeatherForecastContext context,
       ILogger<WeatherForecastController> logger,
       IConfiguration configuration)
     {
       _configuration = configuration;
       _logger = logger;
+      _context = context;
     }
     [HttpGet]
     public IEnumerable<WeatherForecast> Get()
     {
-      return weatherForecasts;
+      return _context.WeatherForecasts
+        //.Where
+        // .Skip
+        // .Take
+        // .OrderBy
+        // .Select
+        ;
     }
     [HttpGet("{id}")]
     public ActionResult<WeatherForecast> Get(int id)
     {
-      var weatherForecast = weatherForecasts.FirstOrDefault( w => w.Id == id);
+      //var weatherForecast = weatherForecasts.FirstOrDefault( w => w.Id == id);
+      var weatherForecast = _context.WeatherForecasts.Find( id);
       if (weatherForecast == null)
         return NotFound();
       else
@@ -60,15 +69,15 @@ namespace DemoRestRentACar.Controllers
     {
       if (!ModelState.IsValid)
         return ValidationProblem(ModelState);
-      weatherForecasts.Add(weatherForecast);
-      weatherForecast.Id = weatherForecasts.Max(x => x.Id)+1;
+      _context.WeatherForecasts.Add(weatherForecast);
+      _context.SaveChangesAsync();
+      Debug.Assert( weatherForecast.Id != 0 );
       return CreatedAtRoute(
           new { controller = nameof(WeatherForecast), action=nameof(Post),
               id = weatherForecast.Id
               },
           weatherForecast
         );
-      //return Ok(weatherForecast);
     }
   }
 }
