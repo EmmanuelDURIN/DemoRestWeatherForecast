d2bdeb2e2151307675aa806f62aa31e4ccbdc223
Emmanuel <emmanueldurin@gmail.com>
2022-01-07 09:53:40 +0100

    Creation de la migration initiale

---
+++ DemoWeatherForeCast/Migrations/20220107082741_InitialCreation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DemoWeatherForeCast.Migrations
+{
+    public partial class InitialCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeatherForecasts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Place = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TemperatureC = table.Column<int>(type: "int", nullable: false),
+                    Summary = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherForecasts", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WeatherForecasts_Place",
+                table: "WeatherForecasts",
+                column: "Place");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherForecasts");
+        }
+    }
+}
